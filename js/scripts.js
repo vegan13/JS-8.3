fde4a769aa9ef2345ac96b4b7055f40f46c5fa0a
vegan13 <vegan13@o2.pl>
2017-02-25 14:38:12 +0100

    added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
+// alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+/* komentarz
+wielo-
+-liniowy*/
+
+// inny komentarz ale jednoliniowy
